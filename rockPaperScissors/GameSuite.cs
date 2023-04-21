1d218989af861b7a4bc7015d0c5dc550a06cfe73
Christian Castillo <christianrafaelwof@gmail.com>
2023-04-21 15:59:51 -0400

    first commit

---
+++ rockPaperScissors/GameSuite.cs
@@ -0,0 +1,307 @@
+﻿namespace GameSuite;
+class Program
+{
+    static void Main(string[] args)
+    {
+    //Console Info
+        Console.Title = "Game Suite";
+
+    //Player Name
+        Console.WriteLine("Enter Player Name:");
+        string P1Name = Console.ReadLine();
+
+    //Program Loop Start 
+        string ProgramLoop = "y";
+        while (ProgramLoop== "y"){
+
+        //Menu Loop
+            Console.WriteLine("SELECT GAME");
+            Console.WriteLine("(1) High Low\n(2) Guess the Number\n(3) Rock Paper Scissors");
+            string GameSelect = Console.ReadLine();
+            while ((GameSelect != "1") && (GameSelect != "2") && (GameSelect != "3")){
+                Console.WriteLine("(1) High Low\n(2) Guess the Number\n(3) Rock Paper Scissors");
+                GameSelect = Console.ReadLine();
+            }
+        
+        //High Low
+            if (GameSelect == "1"){
+                Console.WriteLine ("HIGH LOW");
+                double GameNumber = 0;
+                double GameWins = 0;
+
+    //GameLoop 
+    
+            string GameLoop = "y";
+            while (GameLoop == "y"){
+                Console.WriteLine("High/Low");
+    
+    //CPU Number Gen
+      //First Number
+                Random numberGen = new Random();
+                int CPUChoice1 = 0;
+                CPUChoice1 = numberGen.Next(1,11);
+        //Second NUmber
+                int CPUChoice2 = 0;
+                CPUChoice2 = numberGen.Next(1,11);
+                Console.WriteLine ("First Number: " + CPUChoice1);
+                Console.WriteLine ("Is Second Number Higher(>) or Lower(<)");
+
+        //Player Choice
+                Console.Write (P1Name + ": ");
+                string P1Choice = Console.ReadLine();
+                while ((P1Choice != ">" && P1Choice != "<")){
+                    Console.WriteLine("Enter > or <");
+                    Console.Write (P1Name + ": ");
+                    P1Choice = Console.ReadLine();
+                    }
+                Console.WriteLine(P1Choice);
+
+        //Win Condition
+                if ((CPUChoice1 < CPUChoice2) && (P1Choice == "<") ||(CPUChoice1 > CPUChoice2) && (P1Choice == ">") ){
+                    Console.WriteLine("Win\nSecond Number was: " + CPUChoice2);
+                    GameWins ++;
+                }
+                else if ((CPUChoice1 < CPUChoice2) && (P1Choice == ">") ||(CPUChoice1 > CPUChoice2) && (P1Choice == "<") ){
+                    Console.WriteLine ("Lose\nSecond Number was: " + CPUChoice2);
+                }
+                GameNumber ++;
+                double WinPercent = ((GameWins/GameNumber) * 100);
+                Console.WriteLine ("Game Number: " + GameNumber + "\nGame Wins: " + GameWins);
+                Console.WriteLine ("Win %: " + (Math.Round (WinPercent, 2)) + "%");
+                
+            
+            //Outro
+                    Console.WriteLine("Play Guess the Number Again? (y/n)");
+                    GameLoop = Console.ReadLine();
+                    while ((GameLoop != "y" && GameLoop != "n")){
+                        Console.WriteLine("Enter y or n");
+                        GameLoop = Console.ReadLine();
+                    }            
+
+                }
+
+            }
+
+        //Guess the Number
+                
+                if (GameSelect == "2"){
+                    string GameLoop = "y";
+                    while (GameLoop == "y"){
+            //Intro
+                    Console.WriteLine ("Select game type\n(1) Scored Game\n(2) Free Play");
+                
+            //Game Type Select   
+                    int GameType = Convert.ToInt32 (Console.ReadLine ());
+                    while ((GameType != 1) && (GameType != 2)){
+                        Console.WriteLine("Select 1 or 2");
+                        GameType = Convert.ToInt32 (Console.ReadLine());
+                    }
+                    if (GameType == 1){
+                        Console.WriteLine("SCORED GAME");
+                    }
+                    else if (GameType == 2){
+                        Console.WriteLine("FREE PLAY");
+                    }
+
+            //Game Type Loop Start 
+                    double GameNumber = 0;
+                    double GameWins = 0;
+                    string GameTypeLoop = "y";
+                    while (GameTypeLoop == "y"){ 
+
+            //Number Generator    
+                        Random numberGen = new Random();
+                        int P2Choice = 0;
+                        P2Choice = numberGen.Next(1,101);
+
+            //P1 Turn  
+
+                        Console.WriteLine("Guess the number\n\nTurn Number: 1");
+                        Console.Write (P1Name + ": ");
+                        int P1Choice = Convert.ToInt32 (Console.ReadLine());
+                        while ((0 >= P1Choice) || (101 <= P1Choice)){
+                            Console.WriteLine("Enter Number 1 - 100");
+                            Console.Write (P1Name + ": ");
+                            P1Choice = Convert.ToInt32 (Console.ReadLine());
+                        }
+
+            //Game Code
+                        int Turns = 1;
+
+                        for (int i = 2; (P1Choice != P2Choice) && (i <= 10); i++){
+                        if (GameType == 1){
+                            Turns = i;
+                        }
+                    
+                        else if (GameType == 2){
+                            i = 0;
+                            Turns ++;
+                        }
+
+                        if (P1Choice > P2Choice){
+                            Console.WriteLine ("Lower\n\nTurn Number: " + Turns);
+                            Console.Write (P1Name + ": ");
+                            P1Choice = Convert.ToInt32 (Console.ReadLine());
+                        }
+
+                        else if (P1Choice < P2Choice){
+                            Console.WriteLine ("Higher\n\nTurn Number: " + Turns);
+                            Console.Write (P1Name + ": ");
+                            P1Choice = Convert.ToInt32 (Console.ReadLine());
+                        } 
+
+                    }
+
+            //Win Condition
+
+                    if (P1Choice == P2Choice){
+                        GameNumber ++;
+                        GameWins ++;
+                        Console.WriteLine("Win");
+                    }
+                
+                    else if (Turns == 10 ){
+                        GameNumber ++;
+                        Console.WriteLine("\nLose\nMax Turn Limit Reached");
+                    }
+                    
+                    double WinPercent = ((GameWins/GameNumber) * 100);
+                    Console.WriteLine ("\nTurns Taken: " + Turns);
+                    Console.WriteLine ("Game Number: " + GameNumber + "\nGame Wins: " + GameWins);
+                    Console.WriteLine ("Win %: " + (Math.Round (WinPercent, 2)) + "%");
+            
+            // Game Type Loop End
+                    if (GameType == 1){
+                        Console.WriteLine ("Play Scored Game Again? (y/n)");
+                        GameTypeLoop = Console.ReadLine();
+                        while ((GameTypeLoop != "y" && GameTypeLoop != "n")){
+                        Console.WriteLine("Enter y or n");
+                        GameTypeLoop = Console.ReadLine();
+                        }
+                    }
+            
+                    else if (GameType == 2){
+                        Console.WriteLine ("Play Free Play Game Again? (y/n)");
+                        GameTypeLoop = Console.ReadLine();
+                        while ((GameTypeLoop != "y" && GameTypeLoop != "n")){
+                            Console.WriteLine("Enter y or n");
+                            GameTypeLoop = Console.ReadLine();
+                        }
+                    }
+                }
+        //Game Loop End
+                Console.WriteLine("Play Guess the Number Again? (y/n)");
+                GameLoop = Console.ReadLine();
+                while ((GameLoop != "y" && GameLoop != "n")){
+                    Console.WriteLine("Enter y or n");
+                    GameLoop = Console.ReadLine();
+                    }            
+
+                }
+        
+                }
+        // Rock Paper Scissors
+                if (GameSelect == "3"){
+                    
+                    double GameNumber = 0;
+                    double GameWins = 0;
+    
+                //GameLoop 
+                    string GameLoop = "y";
+                    while (GameLoop == "y"){
+                    
+                        Console.WriteLine ("ROCK PAPER SCISSORS");
+
+                //Game Number Loop
+                        Console.WriteLine ("Best of 3 or 5 games?");
+                        Console.Write (P1Name + ": ");
+                        int GameCount = Convert.ToInt32(Console.ReadLine());
+                        while ((GameCount != 3) && (GameCount!= 5)){
+                            Console.WriteLine("Enter 3 or 5");
+                            GameCount = Convert.ToInt32(Console.ReadLine());
+                        }
+
+                        int P1Wins = 0;
+                        int P2Wins = 0;
+
+                        while ((P1Wins != GameCount) && (P2Wins != GameCount)){
+                    //P2 Number Gen
+                            Random numberGen = new Random();
+                            int P2Choice = 0;
+                            P2Choice = numberGen.Next(1,4);   
+
+
+                    //P1 Turn
+                            Console.WriteLine ("Select (r)Rock, (p)Paper, or (s)Scissors");
+                            Console.Write (P1Name + ": ");
+                            string P1Choice = Console.ReadLine();
+                            while ((P1Choice != "r") && (P1Choice != "p") && (P1Choice != "s")){
+                                    Console.WriteLine("Select (r)Rock, (p)Paper, or (s)Scissors");
+                                    P1Choice = Console.ReadLine();
+                            }
+
+                    // P2 Turn
+                            if (P2Choice == 1){
+                                Console.WriteLine ("CPU: Rock");
+                            }
+                            else if (P2Choice == 2){
+                                Console.WriteLine ("CPU: Paper");
+                            }
+                            else if (P2Choice == 3){
+                                Console.WriteLine ("CPU: Scissors");
+                            }
+                            
+                    //Win Condition
+
+                            if ((P1Choice == "r" && P2Choice == 1) || (P1Choice == "p" && P2Choice == 2) || (P1Choice == "s" && P2Choice == 3)){
+                                Console.WriteLine ("Draw\n");
+                            }
+
+                            else if ((P1Choice == "r" && P2Choice == 2) || (P1Choice == "p" && P2Choice == 3) || (P1Choice == "s" && P2Choice == 1)){
+                                Console.WriteLine ("Lose\n");
+                                P2Wins ++;
+                            }
+
+                            else if ((P1Choice == "r" && P2Choice == 3) || (P1Choice == "p" && P2Choice == 1) || (P1Choice == "s" && P2Choice == 2)){
+                                Console.WriteLine ("Win\n");
+                                GameWins ++;
+                                P1Wins ++;
+                            }
+
+                            Console.WriteLine (P1Name + " W/L: " +  P1Wins + "/" + P2Wins);
+                            if (P1Wins == GameCount){
+                                Console.WriteLine("\n" + P1Name + " Win \n");
+                            }
+                            else if (P2Wins == GameCount){
+                                Console.WriteLine("\nCPU Win \n");
+                            }
+                            
+                            GameNumber ++;
+                            double WinPercent = ((GameWins/GameNumber) * 100);
+                            Console.WriteLine ("Game Number: " + GameNumber + "\nGame Wins: " + GameWins);
+                            Console.WriteLine ("Win %: " + (Math.Round (WinPercent, 2)) + "%");
+                                    
+                        }      
+                //Outro
+                        Console.WriteLine("Play Rock Paper Scissors Again? (y/n)");
+                        GameLoop = Console.ReadLine();
+                        while ((GameLoop != "y" && GameLoop != "n")){
+                            Console.WriteLine("Enter y or n");
+                            GameLoop = Console.ReadLine();
+                        }
+                    }
+                 } 
+                 
+        // Program Loop End
+                }
+                Console.WriteLine("Return to Main Menu? (y/n)");
+                ProgramLoop = Console.ReadLine();
+                while ((ProgramLoop != "y" && ProgramLoop != "n")){
+                    Console.WriteLine("Enter y or n");
+                    ProgramLoop = Console.ReadLine();
+            
+            }
+            Console.WriteLine ("Progress Enter to Terminate");
+            Console.ReadLine();
+        }
+    }
---
+++ rockPaperScissors/Program.cs
@@ -0,0 +1,167 @@
+﻿/*
+
+Summary:A two player game of rock, paper, Scissors where a user will play against the computer.
+Description:Input your name, then choose the winning terms (2 out 3 or 3 out of 5). Then you'll be prompted to choose rock paper or scissors, after your choice the computer chooses one of the three options also. Then the two options are evaluated. Rock beats Scissors,Scissors beats Paper, Paper beats rock. If thier is a tie no point is counted. A point will be added to the player with the winning option. The current score will be shown Username: 0, Computer: 0. Then the options will come up again until the winning conditions are made
+Purpose: practice C# for use in other games. 
+Date Started: 4/12/2023
+Date Modified: 4/13/2023
+Authors: Daniel Wiley & Christian Castillo
+
+*/
+
+using System;
+
+namespace rockPaperScissors
+{
+
+    class Action
+    {
+        public string actionName { get; }
+        public string abbreviation { get; }
+        public string[] actionBeats { get; set; }
+        public string[] actionLosesto { get; set; }
+
+        public Action(string name, string abbrv, string[] beats, string[] losesto)
+        {
+            actionName = name;
+            abbreviation = abbrv;
+            actionBeats = beats;
+            actionLosesto = losesto;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Create all options in game
+            rockPaperScissors.Action rock = new Action("rock", "r", new string[] { "scissors" }, new string[] { "paper" });
+            rockPaperScissors.Action paper = new Action("paper", "p", new string[] { "rock" }, new string[] { "scissors" });
+            rockPaperScissors.Action scissors = new Action("scissors", "s", new string[] { "paper" }, new string[] { "rock" });
+
+            //create array with all actions
+            rockPaperScissors.Action[] actions = { rock, paper, scissors };
+
+
+            //introductions
+            Console.WriteLine("Welcome to rock paper scissors C# Practice!");
+
+            //ask for and store users name.
+            Console.WriteLine("Enter your User name:");
+            string username = Console.ReadLine();
+            string computerplayer = "CPU1";
+
+            //Greet User
+            Console.WriteLine("Hello " + username + "!");
+
+            int p1wins = 0;
+            int p1loses = 0;
+
+
+
+            for (int j = 0; j < 10; j++)
+            {
+
+                Console.WriteLine(username + ": " + p1wins + " vs " + computerplayer + ": " + p1loses);
+
+                if (p1wins == 3 || p1loses == 3)
+                {
+                    Console.WriteLine(username + ": " + p1wins + " vs " + computerplayer + ": " + p1loses);
+
+                    if (p1wins == 3)
+                    {
+                        Console.WriteLine(username + " Wins!");
+                    }
+                    else
+                    {
+                        Console.WriteLine(username + " Loses!");
+                    }
+
+                    break;
+                }
+
+                
+                    //Start sentance (use write instead of writeline so not make a new line.
+                    Console.Write(username + " choose ");
+
+                    //Use the quantity of plays to go through each to write the possible options.
+                    for (int i = 0; i < actions.Length; i++)
+                    {
+                        //Print Play
+                        Console.Write(actions[i].actionName + "(" + actions[i].abbreviation + ")");
+
+                        //If second to last play write or
+                        if (i == (actions.Length - 2))
+                        {
+                            Console.Write(" or ");
+                        }
+                        //Otherwise put a , with a space
+                        else if (i != (actions.Length - 1))
+                        {
+                            Console.Write(", ");
+                        }
+                    }
+                    Console.Write(".");
+                    string actionChosen = (Console.ReadLine());
+                    rockPaperScissors.Action p1action = actions[0];
+
+                    for (int i = 0; i < actions.Length; i++)
+                    {
+
+                        if (actions[i].abbreviation == actionChosen)
+                        {
+                            Console.WriteLine(username + " chose: " + actions[i].actionName);
+                            p1action = actions[i];
+                        }
+
+                    }
+
+                    Random numberGen = new Random();
+                    int p2Choice = 0;
+                    p2Choice = numberGen.Next(0, actions.Length);
+
+                    rockPaperScissors.Action p2action = actions[p2Choice];
+
+                    Console.WriteLine(computerplayer + " chose: " + p2action.actionName);
+
+                    for (int i = 0; i < p1action.actionBeats.Length; i++)
+                    {
+                        if (p1action.actionBeats[i] == p2action.actionName)
+                        {
+                            Console.WriteLine(username + " wins! ");
+                            p1wins++;
+
+                        }
+                        else if (p1action.actionLosesto[i] == p2action.actionName)
+                        {
+                            Console.WriteLine(username + " loses! ");
+                            p1loses++;
+
+                        }
+                        else if (p1action.actionName == p2action.actionName)
+                        {
+                            Console.WriteLine(" DRAW ");
+                        }
+                    }
+
+                
+                //Console.WriteLine("Will you play 2 out of 3 or 3 out of 5?");
+                //Console.WriteLine("Write 1 for play 2 out of 3 or 2 for 3 out of 5?");
+                //get game type 1 or 2 as a int
+                //int gameType = Convert.ToInt32(Console.ReadLine());
+                //scoreToWin will be the used to figure out how many wins are necessary to win
+                //int scoreToWin;
+                //if (gameType == 1) { scoreToWin = 2; } else { scoreToWin = 3; }
+
+                //Intro
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
